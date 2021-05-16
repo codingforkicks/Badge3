b57c347b57eabfac6af6eff46be01e8542288427
Tia Simmons <t.simmons06@yahoo.com>
2021-05-16 19:13:35 -0400

    edit button menu and cancel functionality working

--- Exercises/DVD Library/js/home.js
+++ Exercises/DVD Library/js/home.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 });
 
 function loadDvds() {
-    clearContactTable();
+    clearMovieTable();
     var contentRows = $('#contentRows');
 
     $.ajax({
@@ -23,7 +23,7 @@ function loadDvds() {
                 row += '<td>' + release + '</td>';
                 row += '<td>' + director + '</td>';
                 row += '<td>' + rating + '</td>';
-                row += '<td><button type="button" class="btn btn-link" onclick="editMovie(' + movieId + ')">Edit </button>|<button type="button" class="btn btn-link" onclick="deleteConfirmation(' + movieId + ')">Delete</button></td>';
+                row += '<td><button type="button" class="btn btn-link" onclick="showEditForm(' + movieId + ')">Edit </button>|<button type="button" class="btn btn-link" onclick="deleteConfirmation(' + movieId + ')">Delete</button></td>';
                 row += '</tr>';
 
                 contentRows.append(row);
@@ -60,20 +60,80 @@ function deleteMovie(movieId) {
     });
 }
 
-function hideEditForm() {
+function showEditForm (movieId) {
     $('#errorMessages').empty();
 
-    $('#editFirstName').val('');
-    $('#editLastName').val('');
-    $('#editCompany').val('');
-    $('#editPhone').val('');
-    $('#editEmail').val('');
 
-    $('#contactTableDiv').show();
-    $('#editFormDiv').hide();
+    var editHeader = $('#editHeader');
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://tsg-dvds.herokuapp.com/dvd/' + movieId,
+        success: function (data, status) {
+
+            $('#editTitle').val(data.title);
+            $('#editReleaseYear').val(data.releaseYear);
+            $('#editDirector').val(data.director);
+            $('#editNotes').val(data.notes);
+
+            editHeader.append('Edit Dvd: ' + data.title);
+
+        },
+        error: function () {
+            $('#errorMessages')
+                .append($('<li>')
+                    .attr({ class: 'list-group-item list-group-item-danger' })
+                    .text('Error calling web service. Please try again later.'));
+        }
+    })
+
+    $('#movieData').hide();
+    $('.nav').hide();
+    $('#editDiv').show();
+};
+
+function showInfo(movieId) {
+    $('#errorMessages').empty();
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://tsg-dvds.herokuapp.com/dvd/' + movieId,
+        success: function (data, status) {
+            $('#editTitle').val(data.title);
+            $('#editReleaseYear').val(data.releaseYear);
+            $('#editDirector').val(data.director);
+            $('#editNotes').val(data.notes);
+
+        },
+        error: function () {
+            $('#errorMessages')
+                .append($('<li>')
+                    .attr({ class: 'list-group-item list-group-item-danger' })
+                    .text('Error calling web service. Please try again later.'));
+        }
+    })
+
+    $('#movieData').hide();
+    $('.nav').hide();
+    $('#editDiv').show();
+};
+
+function hideEditForm() {
+    $('#errorMessages').empty();
+    $("#editHeader").empty();
+
+    $('#editTitle').val('');
+    $('#editReleaseYEar').val('');
+    $('#editDirector').val('');
+    $('#editRating').val('');
+    $('#editNotes').val('');
+
+    $('#movieData').show();
+    $('.nav').show();
+    $('#editDiv').hide();
 }
 
 //clear table data
-function clearContactTable() {
+function clearMovieTable() {
     $('#contentRows').empty();
 }
